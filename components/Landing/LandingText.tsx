b70fd8d28045f6629bb794cc575aeb6071b8fc70
Nico Böhner <nico.boehner1999@gmail.com>
2022-09-05 17:30:38 +0200

    Project Section V1

--- components/Landing/LandingText.tsx
+++ components/Landing/LandingText.tsx
@@ -1,31 +1,33 @@
-import { Col, Row } from "react-bootstrap";
-import { useInView } from "react-intersection-observer";
+import {Col, Row} from "react-bootstrap";
+import {useInView} from "react-intersection-observer";
 
 function LandingText() {
-  const { ref: myRef, inView: myElementIsVisible } = useInView();
-  return (
-    <div ref={myRef}>
-      <Col
-        className={
-          myElementIsVisible
-            ? "tablet:fadeInAnimation fadeAnimation tablet:flex tablet:w-[75vh] text-3xl tablet:relative"
-            : "tablet:w-[75vh] text-3xl"
-        }
-      >
-        <div className={"container py-5 z-20 text-center tablet:text-start"}>
-          <Row className="flex tablet:justify-start justify-center tablet:text-7xl text-2xl text-slate-200 font-semibold">
-            Hi! I am Nico.
-          </Row>
-          <Row className="flex tablet:justify-start justify-center tablet:text-7xl text-2xl">
-            <div>
-              <p className="text-brand font-black">A software developer</p>
-              <p className={"text-slate-200 font-semibold"}>from Germany.</p>
-            </div>
-          </Row>
+    const {ref: myRef, inView: myElementIsVisible} = useInView();
+    return (
+        <div ref={myRef}>
+            <Col
+                className={
+                    myElementIsVisible
+                        ? "tablet:fadeInAnimation fadeAnimation tablet:flex tablet:w-[75vh] text-3xl tablet:relative"
+                        : "tablet:w-[75vh] text-3xl"
+                }
+            >
+                <div className={"container py-5 z-20 text-center tablet:text-start text-center"}>
+                    <Row
+                        className="flex tablet:justify-start justify-center tablet:text-7xl text-2xl text-slate-200 font-semibold">
+                        Hi! I am Nico.
+                    </Row>
+                    <Row className="flex tablet:justify-start justify-center tablet:text-7xl text-2xl">
+                        <div>
+                            <p className="text-brand font-black">A software</p>
+                            <p className={"w-full text-start font-semibold"} id={"typeText"}>developer</p>
+                            <p className={"text-slate-200 font-semibold"}>from Germany.</p>
+                        </div>
+                    </Row>
+                </div>
+            </Col>
         </div>
-      </Col>
-    </div>
-  );
+    );
 }
 
 export default LandingText;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type {NextPage} from "next";
 import Header from "../components/Header/Header";
 import NavBar from "../components/NavBar/NavBar";
 import Landing from "../components/Landing/landing";
@@ -6,27 +6,28 @@ import Skills from "../components/Skills";
 import CurriculumVitae from "../components/Curriculum Vitae";
 import SectionSeparator from "../components/Section/SectionSeparator";
 import Contact from "../components/Contact";
+import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Header />
+    return (
+        <>
+            <Header/>
 
-      <NavBar />
+            <NavBar/>
 
-      <Landing />
+            <Landing/>
 
-      <CurriculumVitae />
+            <CurriculumVitae/>
 
-      <SectionSeparator />
+            <Skills/>
 
-      <Skills />
+            <Projects/>
 
-      <SectionSeparator />
+            <SectionSeparator/>
 
-      <Contact />
-    </>
-  );
+            <Contact/>
+        </>
+    );
 };
 
 export default Home;
